997e0807cd430b343b463188c61edc30647e5b7b
HYZENBG <195426046+HyZenBG@users.noreply.github.com>
2025-03-30 10:27:51 +0600

    d

--- main.py
+++ main.py
@@ -97,7 +97,7 @@ def index():
     ''')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(port=5000, debug=True)
 
 # Dockerization support
 # Create a Dockerfile with the following content:
